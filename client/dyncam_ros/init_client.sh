a3b42932fffe23e3751292aa513a91dfb213c4e9
Mayank Sharma <imptodefeat@gmail.com>
2017-11-09 15:38:11 +0100

    create script to run and configure container for client image

--- client/dyncam_ros/init_client.sh
+++ client/dyncam_ros/init_client.sh
@@ -5,16 +5,9 @@ echo "Setting up environment..."
 source "/opt/ros/lunar/setup.bash"
 source "/catkin_ws/devel/setup.bash"
 
-#set up ros network info
-echo "Setting up ros network info"
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=localhost
-export ROS_IP=127.0.0.1
-
 # start viewer
 echo "starting viewer..."
-rosrun kinect2_viewer kinect2_viewer kinect2 sd image
- 
-#exec "$@"
+rosrun kinect2_viewer kinect2_viewer kinect2 sd $STREAM_TYPE
 
-#while true; do sleep 1000; done
+exec "$@"
+
--- client/run_client.sh
+++ client/run_client.sh
@@ -1,3 +1,40 @@
+###############################################################################################
+# 1.Introduction
+# ==============
+#
+# This script will start a docker container for dyncam_ros_benchmark_client image. 
+# Use it on the machine serving as a receiver/client for the streamed data.
+#
+# 2.Pre-requisites
+# ================
+#
+# Install Docker on this machine
+# Make sure a container for dyncam_ros_benchmark_server is running on a different machine on the same network.
+#
+# 3.Usage
+# =======
+#
+# run_client.sh [OPTIONS]
+#
+# 4.Description of OPTIONS
+# =========================
+#
+# (a) --host : IP address or hostname of this machine on the network
+#     
+# (b) --master : IP address of the machine where ros master is running. i.e IP address of the server machine to which kinect is connected. 
+# (c) --ip : IP address of this machine
+# (d) --type [cloud|image] : value can be either 'cloud' or 'image'. Point Cloud will open up a opencv named window displaying the point cloud. 'image' will display a fused matrix of color and depth matrices. 
+# 
+# 5. Example
+# ===========
+#
+# run_clienbt.sh --host=localhost --master=192.168.2.101 --ip=192.168.2.100 --type=cloud
+#
+###############################################################################################
+
+echo "Enabling sharing of display between container and host."
+xhost +local:root #enable sharing of access control list
+
 for i in "$@"
 do
 case $i in
@@ -31,7 +68,20 @@ echo "ROS_HOST       = ${HOST}"
 echo "ROS_MASTER_URL = ${MASTER}"
 echo "ROS_IP         = ${IP}"
 echo "TYPE           = ${STREAM_TYPE}"
-if [[ -n $1 ]]; then
-    echo "Last line of file specified as non-opt/last argument:"
-    tail -1 $1
-fi
+
+sudo docker run -it \
+  --name=client \
+  --rm \
+  --mount type=bind,source=$(pwd)/dyncam_ros,target=/dyncam_ros \
+  --net host \
+  --privileged \
+  -e STREAM_TYPE="${STREAM_TYPE}" \
+  -e DISPLAY=$DISPLAY \
+  -e ROS_MASTER_URI="http://${MASTER}:11311" \
+  -e ROS_HOSTNAME="${HOST}" \
+  -e ROS_IP="${IP}" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /dev/bus/usb:/dev/bus/usb \
+  dyncam_ros_benchmark_client \
+  /bin/bash
+
